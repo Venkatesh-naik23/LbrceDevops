ee316f58953edbfccda915d3a409b5073559f37a
AdmiN <AdmiN@ITWS0041>
2024-08-20 10:56:57 +0530

    commit hello

--- src/H2/Even.java
+++ src/H2/Even.java
@@ -1,11 +1,9 @@
 package H2;
 
-public class welcome {
+public class Even {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("welcome");
 
 	}
 

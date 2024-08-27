f80e80c31af1a470a40a88dcd46d0518e958ec46
AdmiN <AdmiN@ITWS0042>
2024-08-27 11:30:08 +0530

    hello

--- src/H2/Signup.java
+++ src/H2/Signup.java
@@ -1,10 +1,40 @@
 package H2;
+import java.util.Scanner;
+public class Signup 
+{
+    private String username;
+    private String password;
+    private String email;
+    public Signup(String username, String password, String email) 
+    {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 
-public class Signup {
+    public void displayUserInfo() 
+    {
+        System.out.println("User Signed Up Successfully!");
+        System.out.println("Username: " + username);
+        System.out.println("Email: " + email);
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
 
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        System.out.print("Enter email: ");
+        String email = scanner.nextLine();
+
+        Signup signup = new Signup(username, password, email);
+        signup.displayUserInfo();
+
+        scanner.close();
+    }
 }
--- src/H2/welcome.java
+++ src/H2/welcome.java
@@ -6,6 +6,7 @@ public class welcome {
 		// TODO Auto-generated method stub
 		
 		System.out.println("welcome");
+		System.out.println("LBRCE");
 
 	}
 
